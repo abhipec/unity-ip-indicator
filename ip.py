5e65f3d174ce5be7b22abd12d4e15d1edf9005f0
abhishek <abhishek.abhishek.iitg.ac.in>
2015-12-18 18:20:08 +0530

    auto refresh of ip every 5 sec and excluded loopback ip 127.0.0.1

--- ip.py
+++ ip.py
@@ -3,33 +3,31 @@ import os
 import subprocess
 import appindicator
 import gtk
-
+import gobject
 ICON = os.path.abspath("./images/icon.png")
 
 def get_ip():
     ip = subprocess.check_output('ifconfig |\
         grep -o -P "inet addr:([^ ]*)" |\
-        grep -o -m 1 -P "[0-9.]+"', shell=True)
+        grep -o -P "[0-9.]+"', shell=True)
+    ip = ip.replace("127.0.0.1", "")
     return ip.strip()
-
 
 class IPIndicator:
     def __init__(self):
         self.ip = ""
-        self.ind = appindicator.Indicator("ip-indicator", ICON,
-            appindicator.CATEGORY_APPLICATION_STATUS)
+        self.ind = appindicator.Indicator("ip-indicator", ICON, appindicator.CATEGORY_APPLICATION_STATUS)
         self.ind.set_status(appindicator.STATUS_ACTIVE)
         self.update()
         self.ind.set_menu(self.setup_menu())
-    
+        gobject.timeout_add(5000, self.update)
+
     def setup_menu(self):
         menu = gtk.Menu()
-
         refresh = gtk.MenuItem("Refresh")
         refresh.connect("activate", self.on_refresh)
         refresh.show()
         menu.append(refresh)
-
         return menu
 
     def update(self):
@@ -39,9 +37,13 @@ class IPIndicator:
         
         """
         ip = get_ip()
+	if not ip:
+	    ip = "No IP"
         if ip != self.ip:
             self.ip = ip
             self.ind.set_label(ip)
+        # Timeout function requires function to return True
+        return True
 
     def on_refresh(self, widget):
         self.update()
